98a83fdb7b54ce25612eab8c7870d6da9d67d748
Florian Amsallem <amsall_f@epita.fr>
2017-11-12 19:30:39 +0100

    Sup: add

---
+++ Partie2/Sup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Partie2
+{
+    public class Sup : Student
+    {
+        private static ulong nbSup = 0;
+        public Sup(string name, int life, int damage, int physicalArmor, int magicalArmor) : base(name, life, damage, false, physicalArmor, magicalArmor)
+        {
+            nbSup++;
+        }
+
+        public static void DisplayNbSup()
+        {
+            Console.WriteLine("Il y a {0} sups", nbSup);
+        }
+    }
+}
