c81f0f78beef1b4b82960cd7194e6f1d2e3930c1
Florian Amsallem <amsall_f@epita.fr>
2017-11-20 10:01:13 +0100

    Student: fix setters / getters

--- Partie2/Student.cs
+++ Partie2/Student.cs
@@ -4,23 +4,23 @@ namespace Partie2
 {
     public class Student
     {
-        private string name_;
-        private int life_;
-        private int damage_;
-        private bool is_magician_;
-        private int physical_armor_;
-        private int magical_armor_;
+        private string _name;
+        private int _life;
+        private int _damage;
+        public bool _is_magician;
+        public int _physical_armor;
+        public int _magical_armor;
 
         private static ulong nbStudent = 0;
 
         public Student(string name, int life, int damage, bool isMagician, int physicalArmor, int magicalArmor)
         {
-            name_ = name;
-            life_ = life;
-            damage_ = damage;
-            is_magician_ = isMagician;
-            physical_armor_ = physicalArmor;
-            magical_armor_ = magicalArmor;
+            _name = name.ToUpper();
+            _life = life;
+            _damage = damage;
+            _is_magician = isMagician;
+            _physical_armor = physicalArmor;
+            _magical_armor = magicalArmor;
             nbStudent++;
         }
         
@@ -36,53 +36,35 @@ namespace Partie2
         public void TakeDamage(int damage, bool isMagical)
         {
             if (isMagical)
-                this.life_ -= Math.Max(0, damage - this.magical_armor_);
+                this._life -= Math.Max(0, damage - this._magical_armor);
             else
-                this.life_ -= Math.Max(0, damage - this.physical_armor_);
-            this.life_ = Math.Max(0, this.life_);
+                this._life -= Math.Max(0, damage - this._physical_armor);
+            this._life = Math.Max(0, this._life);
         }
         
         public void Attack(Student s)
         {
-            s.TakeDamage(this.damage_, this.is_magician_);
+            s.TakeDamage(this._damage, this._is_magician);
         }
         
         // Getters & Setters
         
         public string Name
         {
-            get { return name_; }
-            set { name_ = value; }
+            get { return _name; }
+            set { _name = value.ToUpper(); }
         }
 
         public int Life
         {
-            get { return life_; }
-            set { life_ = value; }
+            get { return _life; }
+            set { _life = Math.Max(0, value); }
         }
 
         public int Damage
         {
-            get { return damage_; }
-            set { damage_ = value; }
-        }
-
-        public bool IsMagician
-        {
-            get { return is_magician_; }
-            set { is_magician_ = value; }
-        }
-
-        public int PhysicalArmor
-        {
-            get { return physical_armor_; }
-            set { physical_armor_ = value; }
-        }
-
-        public int MagicalArmor
-        {
-            get { return magical_armor_; }
-            set { magical_armor_ = value; }
+            get { return _damage; }
+            set { _damage = Math.Max(Math.Min(value, 10), -10); }
         }
     }
 }
